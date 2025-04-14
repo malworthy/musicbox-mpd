0c702a38670baceb1e50aa15b5290914318cdfee
malcolm worthy <malworthy@gmail.com>
2025-04-14 20:56:29 +1000

    add error handling to playcounter

--- src/musicbox_mpd/playcounter.py
+++ src/musicbox_mpd/playcounter.py
@@ -2,17 +2,45 @@ from mpd import MPDClient
 from mpd.base import CommandError
 import argparse
 import time
+import os
+import pathlib
+import json
 
 
 def get_args():
     parser = argparse.ArgumentParser(
         prog='playcounter-mpd',
         description='Records play counts for songs played on MPD')
-    # parser.add_argument('-v', '--version', action='store_true')
     parser.add_argument('-s', '--server')
     parser.add_argument('-p', '--port')
+    parser.add_argument('--password')
 
     return parser.parse_args()
+
+
+def get_config():
+    default_config = """
+    {
+        "mpd_host" : "localhost",
+        "mpd_port" : 6600
+    }
+    """
+    folder = os.environ.get("SNAP_COMMON")
+    if folder == None:
+        folder = "/etc"
+    config_file = os.path.join(folder, "musicbox-mpd.conf.json")
+
+    if pathlib.Path(config_file).is_file():
+        try:
+            f = open(config_file)
+            config = json.load(f)
+            f.close()
+            return config
+        except Exception as e:
+            print(f"Error loading config file: {e}")
+
+    print("No config file found, using defaults")
+    return json.loads(default_config)
 
 
 def get_play_count(client, uri):
@@ -26,13 +54,17 @@ def get_play_count(client, uri):
         return 0
 
 
-def start():
+def main_loop():
     print("--- Started watching plays ---")
     args = get_args()
-    server = args.server if args.server else "localhost"
-    port = args.port if args.port else 6600
+    config = get_config()
+    server = args.server if args.server else config["mpd_host"]
+    port = args.port if args.port else config["mpd_port"]
+    password = args.password if args.password else config.get("password")
     client = MPDClient()
     client.timeout = 10
+    if password != None:
+        client.password(password)
     client.connect(server, port)
     print(client.mpd_version)
     current_song = ""
@@ -54,3 +86,25 @@ def start():
 
         if status["state"] == "stop":
             current_song = ""
+
+
+def start():
+    backoff = [5, 5, 5, 60 * 5, 60 * 30]
+    error_count = 0
+    while True:
+        try:
+            main_loop()
+        except Exception as e:
+            print(f"Error: {e}")
+            print(f"Retrying in {backoff[error_count]} seconds")
+            time.sleep(backoff[error_count])
+            error_count += 1
+            if error_count >= len(backoff):
+                error_count = len(backoff) - 1
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+
+if __name__ == "__main__":
+    start()
